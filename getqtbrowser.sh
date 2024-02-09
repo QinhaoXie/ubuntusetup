e647c6e8c0bc00417c1e8142b95c8e8ff943ac64
xqh <444039432@qq.com>
2024-02-09 16:27:37 +0800

    add getqtbrowser.sh

---
+++ getqtbrowser.sh
@@ -0,0 +1,5 @@
+apt install unzip wget -y
+wget https://github.com/aindriu80/PyQtWebBrowser/archive/refs/heads/master.zip
+unzip ./master.zip
+pip install --upgrade pip wheel setuptools
+pip install PyQtWebEngine
