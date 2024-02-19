51506ff930ce439569c9a0d76dd29e1f2d5b2419
QinhaoXie <444039432@qq.com>
2024-02-20 05:51:39 +0800

    add proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,6 @@
+
+echo "hostip=$(cat /etc/resolv.conf |grep -oP '(?<=nameserver\ ).*')">>~/.bashrc
+echo "export https_proxy="http://${hostip}:7890"">>~/.bashrc
+echo "export http_proxy="http://${hostip}:7890"">>~/.bashrc
+echo "export all_proxy="socks5://${hostip}:7890"">>~/.bashrc
+source ~/.bashrc
