d50f85e148a1773e6320248c8f9de02c87d78eae
QinhaoXie <444039432@qq.com>
2024-02-20 05:52:39 +0800

    add comment in proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,4 +1,4 @@
-
+# this file set proxy port for wsl2
 echo "hostip=$(cat /etc/resolv.conf |grep -oP '(?<=nameserver\ ).*')">>~/.bashrc
 echo "export https_proxy="http://${hostip}:7890"">>~/.bashrc
 echo "export http_proxy="http://${hostip}:7890"">>~/.bashrc
